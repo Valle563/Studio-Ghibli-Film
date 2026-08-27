7f13457b505837e7c9a38783f80ba597ddaa5264
Valentino Finocchiaro <Valentino.finocchiaro@student.nbi-handelsakademin.se>
2026-08-27 11:55:49 +0200

    done alot with hashrouter and reactrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.tsx"
+import Favorites from "./pages/Favorites.tsx";
+import "./style/index.css"
+
 function App() {
   return (
-    <h1>Hello</h1>
+    <HashRouter>
+      <div className="app">
+      <header className="header">
+        <h1> Go Ghibli </h1>
+        <nav>
+          <Link to="/"> Filmer </Link>
+          <Link to="/Favoriter"> Favoriter </Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/Favoriter" element={<Favorites />}/>
+      </Routes>
+      </div>
+    </HashRouter>
   )
 }
 
-export default App
+export default App 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2> Home - alla filmer </h2>
+    </div>
+  )
+}
+
+export default Home
